2cfa366b5e0c73898d2da3ef5340263f48f0b366
Phil Burnier <phil@llt-group.com>
2015-03-25 11:50:46 -0500

    php template initial upload

---
+++ header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+	<link rel="stylesheet" href="style.css">
+	<link rel="icon" type="image/png" href="favicon.png">
+	<title>Title Here</title>
+	<!--[if IE]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+	<div class="mobile-nav">
+		
+	</div>
+
+	<div class="header">
+		<div class="container">
+			
+			<div class="logo">
+				<a href="index.php">
+					<img src="images/logo.png" alt="">
+				</a>
+			</div>
+
+			<div class="nav">
+				
+			</div>
+
+			<div class="toggle-menu">
+				
+			</div>
+
+		</div>
+	</div>
