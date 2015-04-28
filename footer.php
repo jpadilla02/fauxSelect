c2625a929461557a6a61fbb1ba0e58ca989347c4
Phil Burnier <phil@llt-group.com>
2015-04-28 09:14:40 -0500

    included actual LLT markup

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
 
 	<div class="footer">
-		[-- insert copyright and designed/developed by llt --] DO IT NOW (jk, but seriously)
+		<p>LLT Group - <a target="_blank" href="http://www.llt-group.com">Website Design &amp; Creative Agency</a></p>
 	</div>
 
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
