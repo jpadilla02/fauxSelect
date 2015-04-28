e086c647989a13dc73a18673d8060906f8910071
Phil Burnier <phil@llt-group.com>
2015-04-28 09:04:50 -0500

    friendly reminder to include footer tags

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
 
 	<div class="footer">
-		
+		[-- insert copyright and designed/developed by llt --] DO IT NOW (jk, but seriously)
 	</div>
 
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
