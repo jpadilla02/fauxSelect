cf6b63dfa2c7b2b24574b80d2aef2093d2a8afc5
Phil Burnier <phil@llt-group.com>
2015-04-28 09:16:35 -0500

    included actual LLT markup

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
 
 	<div class="footer">
-		<p>LLT Group - <a target="_blank" href="http://www.llt-group.com">Website Design &amp; Creative Agency</a></p>
+		<p>LLT Group - <a target="_blank" href="http://www.llt-group.com">Website Design</a> &amp; <a target="_blank" href="http://www.llt-group.com">Creative Agency</a></p>
 	</div>
 
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
