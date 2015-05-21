1f7171b80771e6dd6dfb226a1d1def2ec89bf157
Phil Burnier <phil@llt-group.com>
2015-05-21 15:11:14 -0500

    added mulitply mixin

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,21 @@
 jQuery(document).ready(function($){
 
+	/*************************
+	
+	 STYLE DIRECTORY
+	
+		1. =Title
+	
+	*************************/
 	
 	
+	//=====================================================//
+	// =Title
+	//=====================================================//
+	
+
+	// tests for IE and appends overlayFix in cases for multiplied background
+	// if( /*@cc_on!@*/false || !!document.documentMode ){
+		// $('.sample-element').append('<div class="overlayFix"/>');
+	// }
 });
