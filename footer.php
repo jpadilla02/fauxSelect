407ad8125bb9363997da8c450d763526a4ceadce
Phil Burnier <phil@llt-group.com>
2015-05-21 15:25:10 -0500

    added commented out jquery mobile link in the footer

--- footer.php
+++ footer.php
@@ -7,6 +7,7 @@
 	</div>
 
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<!-- // <script type="text/javascript" src="js/jquery.mobile.js"></script> -->
 	<script type="text/javascript" src="js/functions.js"></script>
 </body>
 
